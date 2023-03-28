4ed8c050bb53099ef57f7708b9cdd6003d5145db
SaheedKazeem <saheekazeem@gmail.com>
2023-03-28 06:40:38 +0100

    fixed crouch added tags

--- CMVS/Assets/Scripts/SlimeMobScript.cs
+++ CMVS/Assets/Scripts/SlimeMobScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SlimeMobScript : MonoBehaviour
 {
-    public float knockbackForce = 100f;
+    private float knockbackForce = 180f;
     public int maxHealth = 100;
     int currentHealth;
     public float speed;
@@ -13,10 +13,6 @@ public class SlimeMobScript : MonoBehaviour
     public float minimumDistance;
     public PlayerCombatScript RefToPlayerCombatScript;
     public Animator RefToAnim;
-
-    // New variable to track if the slime has already damaged the player in the current collision
-    private bool hasDamagedPlayer = false;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,36 +20,31 @@ public class SlimeMobScript : MonoBehaviour
         reftoSpeed = speed;
     }
 
-   private void OnCollisionStay2D(Collision2D other)
-   {
-       if (other.gameObject.CompareTag("Player") && !hasDamagedPlayer)
-       {
-           // Release the object from freeze constraints
-           GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Release the object from freeze constraints
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
 
-           // Calculate knockback direction
-           Vector2 knockbackDir = transform.position - other.transform.position;
+            // Calculate knockback direction
+            Vector2 knockbackDir = transform.position - other.transform.position;
 
-           // Apply knockback force
-           GetComponent<Rigidbody2D>().AddForce(knockbackDir.normalized * knockbackForce, ForceMode2D.Impulse);
+            // Apply knockback force
+            GetComponent<Rigidbody2D>().AddForce(knockbackDir.normalized * knockbackForce, ForceMode2D.Impulse);
 
-           // Attack Code
-           RefToPlayerCombatScript.TakeDamage(30);
+            // Attack Code
+            RefToPlayerCombatScript.TakeDamage(30);
+        }
 
-           // Set the hasDamagedPlayer variable to true so that the slime won't damage the player again in this collision
-           hasDamagedPlayer = true;
-       }
-   }
 
-   void OnCollisionExit2D(Collision2D other)
-   {
-       // Lock the object again
-    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-    
-    
-       // Reset the hasDamagedPlayer variable when the collision ends
-       hasDamagedPlayer = false;
-   }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        // Lock the object again
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
 
     public void TakeDamage(int damage)
     {
@@ -78,7 +69,7 @@ public class SlimeMobScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Vector2.Distance(transform.position, target.position) > minimumDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
@@ -88,4 +79,5 @@ public class SlimeMobScript : MonoBehaviour
             transform.position = new Vector2(transform.position.x, 56f);
         }
     }
+
 }
